5ea76d79ad9f012d05b2e1dfa0ce9321659bb77e
Danila Bokun <d.bokun@yandex.ru>
2023-07-25 15:49:46 +0400

    23 BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import webpack from 'webpack'
 import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins (
   { paths, isDev }: BuildOptions
@@ -19,6 +20,9 @@ export function buildPlugins (
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    })
   ]
 }
