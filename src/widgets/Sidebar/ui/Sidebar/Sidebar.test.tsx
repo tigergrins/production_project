f7c310bc353144e9110a0340d8fb2921d00ee3a9
Danila Bokun <d.bokun@yandex.ru>
2023-08-27 17:04:49 +0400

    28 fixed test Sidebar & added reg-cli & approve ui test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
+import { componentRender } from 'shared/lib/test/componentsRender/componentsRender'
 import { Sidebar } from './Sidebar'
-import {
-  renderWithTranslation
-} from '../../../../shared/lib/test/renderWithTranslation/renderWithTranslation'
 
 describe('Sidebar', () => {
   test('Test render', () => {
-    renderWithTranslation(<Sidebar/>)
+    componentRender(<Sidebar/>)
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('Test toggle', () => {
-    renderWithTranslation(<Sidebar/>)
+    componentRender(<Sidebar/>)
     const toggleBtn = screen.getByTestId('sidebar-toggle')
     fireEvent.click(toggleBtn)
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
